c78c7d2d18709a74372707390c2b97372dba6b3a
Yar Rick <yar@yareg.com>
2020-01-13 17:27:31 +0300

    fb & ok updated

--- src/services/OdnoklassnikiOAuth2Service.php
+++ src/services/OdnoklassnikiOAuth2Service.php
@@ -27,13 +27,13 @@ class OdnoklassnikiOAuth2Service extends Service
 	const SCOPE_SET_STATUS = 'SET STATUS';
 	const SCOPE_PHOTO_CONTENT = 'PHOTO CONTENT';
 
-	protected $name = 'odnoklassniki';
+	protected $name = 'ok';
 	protected $title = 'Odnoklassniki';
 	protected $type = 'OAuth2';
 	protected $jsArguments = ['popup' => ['width' => 680, 'height' => 500]];
 
 	protected $clientPublic;
-	protected $scopes = [];
+	//protected $scopes = [];
 	protected $scopeSeparator = ';';
 	protected $providerOptions = [
             'authorize'    => 'https://connect.ok.ru/oauth/authorize',
